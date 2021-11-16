da316605c34eafa0790b24911e51d9ed71294c93
Derwin <darwin.arenas@epfac.edu.co>
2021-11-16 10:57:22 -0500

    "commit desde vscode"

--- test_src/test_make_a_dict.py
+++ test_src/test_make_a_dict.py
@@ -1,6 +1,5 @@
 import pytest
 from src.make_a_dict import make_a_dict
-from src.persona import persona
 
 class TestMakeADict:
 
@@ -10,8 +9,12 @@ class TestMakeADict:
 
         assert my_dict == expected_dict
 
-
+from src.make_a_dict import persona
 class TestPersona:
+
+    @pytest.fixture
+    def persona():
+        return Persona(nombre="foo", edad=30, profesion='contador')
 
     def test_years_until_retirement(self):
         assert year_until_retirement(persona) == 35
@@ -19,6 +22,8 @@ class TestPersona:
 
     def test_name_and_age(self):
         assert name_and_age(persona) == ('foo', 30)
+
+
 
 if __name__ == '__main__':
     pytest.main()
